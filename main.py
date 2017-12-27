5f3f7149905042646d1ac021deb36158088ff96e
Jace <b436412@gmail.com>
2017-12-27 00:54:08 -0800

    Create project

---
+++ main.py
@@ -0,0 +1,24 @@
+# main function for FaceExpress data formater
+
+
+fps = 24
+inputPath = ""
+outputPath = ""
+
+
+
+def getData()
+
+
+def outputData()
+
+
+def main()
+    
+    # trigger a job for X time in 1 sec to match up the FPS rate.
+
+    # get input data.
+
+    # format....
+
+    # output data...
